f34542f623552d445bc4f510ee65156eea6a24cc
sh3devil <83975576+sh3devil@users.noreply.github.com>
2023-10-20 14:17:40 +0100

    Update index.tsx

--- src/plugins/faeffect/index.tsx
+++ src/plugins/faeffect/index.tsx
@@ -31,23 +31,6 @@ interface UserProfile extends User {
     themeColors?: Array<number>;
 }
 
-interface Colors {
-    primary: number;
-    accent: number;
-}
-
-function encode(primary: number, accent: number): string {
-    const message = `[#${primary.toString(16).padStart(6, "0")},#${accent.toString(16).padStart(6, "0")}]`;
-    const padding = "";
-    const encoded = Array.from(message)
-        .map(x => x.codePointAt(0))
-        .filter(x => x! >= 0x20 && x! <= 0x7f)
-        .map(x => String.fromCodePoint(x! + 0xe0000))
-        .join("");
-
-    return (padding || "") + " " + encoded;
-}
-
 var faeffect;
 var faeframe;
 
@@ -59,32 +42,17 @@ function decode(bio: string): Array<number> | null {
         /ff:/
         );
     faeffect = /fe:"(.*?)"/.exec(bio)?.[1];
-    
-    const colorString = bio.match(
-        /\u{e005b}\u{e0023}([\u{e0061}-\u{e0066}\u{e0041}-\u{e0046}\u{e0030}-\u{e0039}]+?)\u{e002c}\u{e0023}([\u{e0061}-\u{e0066}\u{e0041}-\u{e0046}\u{e0030}-\u{e0039}]+?)\u{e005d}/u,
-    );
-    if (colorString != null) {
-        const parsed = [...colorString[0]]
-            .map(x => String.fromCodePoint(x.codePointAt(0)! - 0xe0000))
-            .join("");
-        const colors = parsed
-            .substring(1, parsed.length - 1)
-            .split(",")
-            .map(x => parseInt(x.replace("#", "0x"), 16));
-
-        return colors;
-    } else {
-        return null;
-    }
+    return faeffect
 }
+    
 
 const settings = definePluginSettings({
     nitroFirst: {
-        description: "Default color source if both are present",
+        description: "-",
         type: OptionType.SELECT,
         options: [
-            { label: "Nitro colors", value: true, default: true },
-            { label: "Fake colors", value: false },
+            { label: "-", value: true, default: true },
+            { label: "-", value: false },
         ]
     }
 });
@@ -126,28 +94,15 @@ export default definePlugin({
     colorDecodeHook(user: UserProfile) {
         if (user) {
             // don't replace colors if already set with nitro
-            if (settings.store.nitroFirst && user.themeColors) return user;
+            if (settings.store.nitroFirst && user.profileEffectID) return user;
             const colors = decode(user.bio);
             if (colors) {
                 return virtualMerge(user, {
                     premiumType: 2,
-                    themeColors: colors,
                     profileEffectID: faeffect
                 });
             }
         }
         return user;
-    },
-    addCopy3y3Button: ErrorBoundary.wrap(function ({ primary, accent }: Colors) {
-        return <Button
-            onClick={() => {
-                const colorString = encode(primary, accent);
-                copyWithToast(colorString);
-            }}
-            color={Button.Colors.PRIMARY}
-            size={Button.Sizes.XLARGE}
-            className={Margins.left16}
-        >Copy 3y3
-        </Button >;
-    }, { noop: true }),
+    }
 });
